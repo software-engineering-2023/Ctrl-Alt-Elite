810dd57605d5b7c11df9aaa50c8285968de630b4
Seif Hany <seif.naguib@gmail.com>
2023-05-28 15:23:47 +0300

    setup navbar

--- nile-delta-bank/src/Routes/paths.js
+++ nile-delta-bank/src/Routes/paths.js
@@ -1,5 +1,4 @@
 export const HOME_PATH = "/";
 export const LOGIN_PATH = "/login";
 export const REGISTER_PATH = "/register";
-
-export const LANDING_PATH = "/landing";
+export const CLIENT_PATH = "/client";
---
+++ nile-delta-bank/src/Screens/Client Page/ClientPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavMenu from '../../components/NavMenu';
+
+const ClientPage = () => {
+  return <div>
+    <NavMenu >
+      
+    </NavMenu>
+  </div>;
+}
+
+export default ClientPage;
