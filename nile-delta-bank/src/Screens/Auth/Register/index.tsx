60a46f2c3d7e09084c0dd9657e0b9ea6f0663c1f
Youssef Ayman <youssefabuzaid@hotmail.com>
2023-05-28 21:20:23 +0300

    aasa

--- nile-delta-bank/src/Screens/Auth/Register/index.tsx
+++ nile-delta-bank/src/Screens/Auth/Register/index.tsx
@@ -1,7 +1,42 @@
-
+import "../../../css/register-page.css";
+import { Input } from "antd";
 const Register = () => {
+
     return (
-        <h1>Register</h1>
+        <div style={{ width: "100%", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <div className="register-card">
+                <div className="register-icon">
+                    <img src="res/registeration/login-illustration.svg" />
+                </div>
+                <div className="right-section">
+                    <div className="gay-part">
+                        <div className="mini-bank-icon">
+                            <img style={{ width: "4rem", height: "4rem" }} src="res/registeration/Mini-Bank-Logo.svg" />
+                        </div>
+                        <div style={{ marginLeft: "4rem", marginBottom: "4rem" }}>
+                            <h1 className="kalam-big">Registration</h1>
+                            <p className="kalam-small">Join Nile Delta Bank!</p>
+                        </div>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                            }}
+                        >
+                            <div
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    rowGap: 3
+                                }}
+                            >
+                            </div>
+                            <Input></Input>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- nile-delta-bank/src/index.tsx
+++ nile-delta-bank/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppRoutes from './Routes/AppRoutes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AppRoutes />
+    </Router>
   </React.StrictMode>
 );
 
