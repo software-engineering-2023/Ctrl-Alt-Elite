0d2e1f065cc74cee43da82d4b2d47866b943b9cc
Seif Hany <seif.naguib@gmail.com>
2023-05-29 16:10:48 +0300

    created account cards and animated click

--- nile-delta-bank/src/Screens/Client Page/My Accounts/AllAccountsPage.tsx
+++ nile-delta-bank/src/Screens/Client Page/My Accounts/AllAccountsPage.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import AccountCard from '../../../components/AccountCard'
+import { savings_account, current_account } from '../../../data/account-cards'
 
 function AllAccountsPage() {
   return (
-    <div>AllAccountsPage.tsx</div>
+    <div>
+      <section className="client_section">
+        <h2>Savings Accounts</h2>
+        <hr className="loan_header_divider" />
+
+        <div className="account_card_items">
+          {
+            savings_account.map((account, index) => {
+              return <AccountCard title={account['title']} number={account['number']} />
+            })
+          }
+        </div>
+      </section>
+      <section className="client_section">
+        <h2>Current Accounts</h2>
+        <hr className="loan_header_divider" />
+
+        <div className="account_card_items">
+          {
+            current_account.map((account, index) => {
+              return <AccountCard title={account['title']} number={account['number']} />
+            })
+          }
+        </div>
+      </section>
+    </div>
   )
 }
 
