0d21cf3ae1eee1f7ae8a72c154168aaeea4a79ec
Seif Hany <seif.naguib@gmail.com>
2023-05-29 05:03:42 +0300

    basic loans and credit cards page done

--- nile-delta-bank/src/Screens/Client Page/Credit Cards/CreditCardRequestsPage.tsx
+++ nile-delta-bank/src/Screens/Client Page/Credit Cards/CreditCardRequestsPage.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import RequestItem from '../../../components/RequestItem'
+import { card_requests } from '../../../data/card_requests'
 
 function CreditCardRequestsPage() {
   return (
-    <div>CreditCardRequestsPage.tsx</div>
+    <section className="client_section">
+      <h2>Credit Card Requests</h2>
+      <hr className="loan_header_divider" />
+
+      {
+        card_requests.map((request, index) => {
+          return (
+            <RequestItem
+              request_number={request.request_number}
+              submit_date={request.submit_date}
+              current={request.current}
+              items={request.items}
+            />
+          )
+        })
+      }
+    </section>
   )
 }
 
